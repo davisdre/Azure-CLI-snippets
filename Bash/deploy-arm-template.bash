573502b83f5cba65384cf0c747c9dfb7e7dd493a
Drew Davis <davisdre@hotmail.com>
2022-12-01 15:25:48 -0500

    Create deploy-arm-template.bash

---
+++ Bash/deploy-arm-template.bash
@@ -0,0 +1,5 @@
+# Deploy a arm template from a json file that you have already created.
+az deployment group create `
+--resource-group (az group list --query [].name -o tsv) `
+--name CLIDEPLOY `
+--template-file linuxvm.json
