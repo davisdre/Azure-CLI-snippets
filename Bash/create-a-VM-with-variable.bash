e13efd2e095610a64b220e14ddd783e1b60322d4
Drew Davis <davisdre@hotmail.com>
2022-12-02 16:40:10 -0500

    updated

--- Bash/create-a-VM-with-variable.bash
+++ Bash/create-a-VM-with-variable.bash
@@ -2,9 +2,9 @@
 $RG = az group list --query [].name -o tsv
 
 # create a linux VM.
-az vm create `
---resource-group $RG `
---name vm-demo-01 `
---image UbuntuLTS `
---admin-username clouddrew '
+az vm create \
+--resource-group $RG \
+--name vm-demo-01 \
+--image UbuntuLTS \
+--admin-username clouddrew \
 --generate-ssh-keys
